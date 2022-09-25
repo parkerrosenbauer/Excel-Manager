61f0f3a5f12c9a3195e585e9b1524b5934e28771
Parker Rosenbauer <parkerrosenbauer@gmail.com>
2022-09-25 11:44:12 -0500

    Update excel.py

--- excel.py
+++ excel.py
@@ -1,35 +1,38 @@
 import pandas as pd
+from pathlib import Path
 
-class ExcelMgr():
-    def __init__(self, path: str):
-        """Create excel object.
-
-        path -- path to excel file
-        """
+class ExcelMgr:
+    def __init__(self, path: Path) -> None:
         self.path = path
-        
-    @property
-    def sheets(self):
-        xl = pd.ExcelFile(self.path)
-        return list(xl.sheet_names)
-        
-    def __contains__(self, sheet: str):
-        """Return True if sheet is in the excel file."""
+
+    def __contains__(self, sheet: str) -> bool:
         return sheet in self.sheets
 
-class SheetMgr(ExcelMgr):
-    def __init__(self, path: str, sheet: str):
-        """Create excel sheet object.
+    @property
+    def sheets(self) -> list:
+        xl = pd.ExcelFile(self.path)
+        return (list(xl.sheet_names))
 
-        path -- path to excel file
-        sheet -- sheet in excel file
-        """
-        super().__init__(path)
+
+class SheetMgr():
+    def __init__(self, path: Path, sheet: str) -> None:
+        self.path = path
         self.sheet = sheet
-        self.data = pd.read_excel(self.path, sheet_name=self.sheet)
-        self.columns = list(self.data.columns)
-        self.record_count = len(self.data.index)
 
-    def column_row_count(self, column: str):
-        """Return count of non null values in the specified column."""
+    def __contains__(self, col: str) -> bool:
+        return col in self.columns
+
+    @property
+    def data(self) -> pd.DataFrame:
+        return pd.read_excel(self.path, sheet_name=self.sheet)
+
+    @property
+    def record_count(self) -> int:
+        return len(self.data.index)
+
+    @property
+    def columns(self) -> list:
+        return list(self.data.columns)
+
+    def column_row_count(self, column: str) -> int:
         return self.data[column].count()
