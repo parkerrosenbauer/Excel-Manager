7e5ffc129e744170d15a5f030da602b9ed1c49de
Parker Rosenbauer <parkerrosenbauer@gmail.com>
2022-09-25 13:25:18 -0500

    Update excel.py

--- excel.py
+++ excel.py
@@ -3,36 +3,84 @@ from pathlib import Path
 
 class ExcelMgr:
     def __init__(self, path: Path) -> None:
+        """Create ExcelMgr object.
+
+        path -- path to excel file
+        """
         self.path = path
 
     def __contains__(self, sheet: str) -> bool:
+        """Return true if sheet present in excel file.
+
+        sheet -- sheet to search for
+        """
         return sheet in self.sheets
+
+    def __getattr__(self, attr):
+        """Return attribute or SheetMgr object when using dot notation."""
+        attribute = attr in self.__dict__.keys()
+        worksheet = attr in self.sheets
+
+        if attribute and worksheet:
+            raise ReferenceError(
+                f'{attr} references both an ExcelMgr attribute and a sheet in the excel file.')
+        elif attribute:
+            return attr
+        else:
+            # if the sheet is not a worksheet, the SheetMgr class will throw an error
+            return SheetMgr(self.path, attr)
+
+    def __getitem__(self, item):
+        """Return SheetMgr object when using brackets."""
+        if item in self.__dict__.keys():
+            raise ReferenceError(
+                f'{item} is an attribute, not a sheet in the file. Use dot notation to access this objects attributes.')
+        else:
+            # if the sheet is not a worksheet, the SheetMgr class will throw an error
+            return SheetMgr(self.path, item)
 
     @property
     def sheets(self) -> list:
+        """Return sheets in excel file"""
         xl = pd.ExcelFile(self.path)
         return (list(xl.sheet_names))
 
 
-class SheetMgr():
+class SheetMgr(ExcelMgr):
     def __init__(self, path: Path, sheet: str) -> None:
-        self.path = path
-        self.sheet = sheet
+        """Create SheetMgr object.
 
-    def __contains__(self, col: str) -> bool:
-        return col in self.columns
+        path -- path to excel file
+        sheet -- sheet to read from
+        """
+        super().__init__(path)
+        if sheet in self.sheets:
+            self.sheet = sheet
+        else:
+            raise LookupError('That sheet name does not exist in file.')
+
+    def __contains__(self, column: str) -> bool:
+        """Return True if column present in sheet."""
+        return column in self.columns
+
+    @property
+    def columns(self) -> list:
+        """Return list of columns in sheet."""
+        return list(self.data.columns)
 
     @property
     def data(self) -> pd.DataFrame:
+        """Return a dataframe of the sheet"""
         return pd.read_excel(self.path, sheet_name=self.sheet)
 
     @property
     def record_count(self) -> int:
+        """Return count of rows in sheet"""
         return len(self.data.index)
 
-    @property
-    def columns(self) -> list:
-        return list(self.data.columns)
-
     def column_row_count(self, column: str) -> int:
+        """Return count of items in column.
+        
+        column -- column containing items
+        """
         return self.data[column].count()
